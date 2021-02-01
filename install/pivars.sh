d96245e1334d06f33022846402d98748d11eaca3
julianes10 <julianes10@yahoo.com>
2021-02-01 07:10:00 +0100

    creting skeleton for pi core

--- install/pivars.sh
+++ install/pivars.sh
@@ -1,18 +1,12 @@
 #!/bin/bash 
-PROJECT_NAME=pipapi
+PROJECT_NAME=pitv
 DEPLOY_FOLDER=/opt/$PROJECT_NAME
-#SERVICES_LIST="simpleDHTtracker kodiAgent webStreamingAgent"
-SERVICES_LIST="pipapi systemStatus simpleDHTtracker webStreamingAgent telegramBOT mjpg-streamer  picam timelapse"
+SERVICES_LIST="kodi systemStatus webStreamingAgent telegramBOT"
+#SERVICES_LIST="systemStatus webStreamingAgent telegramBOT"
 PI_USER=pi
 
-PI_PORT=9177
-PI_IPNAME=ciriacostar.ddns.net
-#PI_IPNAME=192.168.1.133
-# OMEGA PI_IPNAME=192.168.1.55
-#PI_IPNAME=192.168.1.15  #LADERA
-#PI_PORT=22
-#PI_IPNAME=192.168.1.63
-
+PI_IPNAME=192.168.1.55
+PI_PORT=22
 
 
 
